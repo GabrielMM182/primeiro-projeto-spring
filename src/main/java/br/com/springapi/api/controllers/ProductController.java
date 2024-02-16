cb4a0048dead3dde82fab97a99b054699e5dc90b
GabrielMM182 <gmm_morais@hotmail.com>
2024-02-16 16:27:34 -0300

    endpoint post, delete e update feitos

--- src/main/java/br/com/springapi/api/controllers/ProductController.java
+++ src/main/java/br/com/springapi/api/controllers/ProductController.java
@@ -6,13 +6,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springapi.api.models.Product;
 import br.com.springapi.api.repositories.ProductRepository;
+import br.com.springapi.api.utils.ResponseHandler;
 
 @RestController
 @RequestMapping("/products")
@@ -27,15 +32,49 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable Integer id) { // utilizar parametros para busca
+    public ResponseEntity<Object> getProduct(@PathVariable Integer id) { // utilizar parametros para busca
         Optional<Product> product = productRepository.findById(id); // como o id pode não existir vamos usar o Optional
 
         if (!product.isPresent()) {
-            // return ResponseHandler.generate("product not found", HttpStatus.NOT_FOUND);
-            return ResponseEntity.notFound().build();
+            return ResponseHandler.generate("Produto não encontrado", HttpStatus.NOT_FOUND);
         }
 
-        return new ResponseEntity<Product>(product.get(), HttpStatus.OK);
+        return new ResponseEntity<Object>(product.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        Product newProduct = productRepository.save(product);
+        return new ResponseEntity<Object>(newProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody Product product) {
+        Optional<Product> oldProduct = productRepository.findById(id);
+
+        if (!oldProduct.isPresent()) {
+            return ResponseHandler.generate("Produto não encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        Product updateProduct = oldProduct.get();
+        updateProduct.setName(product.getName());
+        updateProduct.setDescription(product.getDescription());
+        updateProduct.setPrice(product.getPrice());
+
+        productRepository.save(updateProduct);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Integer id) {
+        Optional<Product> oldProduct = productRepository.findById(id);
+
+        if (!oldProduct.isPresent()) {
+            return ResponseHandler.generate("Produto não encontrado", HttpStatus.NOT_FOUND);
+        }
+
+        productRepository.delete(oldProduct.get());
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/com/springapi/api/models/Product.java
+++ src/main/java/br/com/springapi/api/models/Product.java
@@ -5,20 +5,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Product {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     
+    @Getter
+    @Setter
     @Column(nullable = false)
     private String name;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private int price;
 
+    @Getter
+    @Setter
     @Column()
     private String description;
     
